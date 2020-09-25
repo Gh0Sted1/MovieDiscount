d9fff68a625f4b36f4a86ec6a913887c99e41585
Gh0Sted1 <gryphen.clark336@sccdsb.net>
2020-09-25 14:09:59 -0400

    created own

--- MovieDiscount.java
+++ MovieDiscount.java
@@ -1,7 +1,7 @@
 /*
- * Programmer:
- * Date:
- * Purpose:
+ /* Programmer: Gryphen Clark
+ * Date: September 25th 2020
+ * Purpose: To make a program that allows to discount users on their age and if they have a coupon.
  */
 
 import java.util.Scanner;
@@ -11,15 +11,29 @@ public class MovieDiscount {
         // Create the input scanner
         Scanner input = new Scanner(System.in);
 
-        // Ask the user for the side lengths
+        // Ask the user for their age 
         System.err.print("How old are you? ");
         int age = input.nextInt();
 
-        // Ask the user if they have a coupon
+        // Ask the user if they have a coupon. This would read in a true or false statement.
         System.err.print("Do you have a coupon? [true/false] ");
-        // TODO: Read in a true/false value
-
+        boolean hasCoupon = input.nextBoolean();
+        
         // Print the cost of a movie ticket
-        // TODO: Figure out the cost and print it
+        if (age <= 12 || age >= 65) {
+        	if(hasCoupon == true) {
+        		System.out.println("Your ticket costs $9.50");
+        	}
+        	else {
+        		if(hasCoupon = false)
+        		System.out.println("Your ticket costs $11.50.");
+        	}
+        	
+        }	
+        else {
+        	if(hasCoupon = true) {
+        		System.out.println("Your ticket costs $12.50");
+        	}
+        }
     }
 }
